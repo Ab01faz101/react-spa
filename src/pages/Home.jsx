8b27271be95c48ae102daea19722122d1326363e
reza <ar.rahmatyy@gmail.com>
2026-02-22 05:38:24 -0800

    setup project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    console.log("hi")
+    return (
+        <>
+            <h1 className="bg-gray-900 py-10 rounded-md text-white w-full text-center">Hi React!</h1>
+        </>
+    )
+}
+
+export default Home;
